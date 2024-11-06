6e03249a039d738dd47899651c9b8775e1305539
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-06 13:27:32 +0530

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -1,43 +1,40 @@
 const socket = io()
 let name;
+
 do {
     name = prompt("Enter your name:")
 } while (!name)
 
 const chatForm = document.getElementById('chat-form')
 const msgContainer = document.querySelector('.msg-container')
-const input  =  document.getElementById('input-msg')
-
-// const chatMessages = document.querySelector('.msg-content')
-// const users = document.querySelector('user')
+const input = document.getElementById('input-msg')
 
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault()
     sendMsg(input.value.trim())
 })
 
-sendMsg = (message) => {
-    let msg {
+const sendMsg = (message) => {
+    let msg = {
         user: name,
         message: message
     }
-    
-    socket.emit('send', msg)
-    appendMsg(msg, 'outgoing')
+
+    socket.emit('send', msg)  // Send the message to the server
+    appendMsg(msg, 'outgoing-msg')  // Append the outgoing message to the DOM
+    input.value = ''
 }
 
-appendMsg = (msg, type) => {
+const appendMsg = (msg, type) => {
     const msgContent = document.createElement('div')
-    msgContent.classList.add(type,'msg')
+    msgContent.classList.add(type, 'msg')
     msgContent.innerHTML = `
-        <span class="absolute -top-10 right-0 text-sm  sender font-light">${msg.user}</span>
-        <span class="font-normal  messgae-content  text-lg text-white p-2">${msg.message}</span>
+        <span class="absolute -top-5 right-0 text-lg md:text-sm text-slate-200 sender font-light" >${msg.user}</span>
+         <span class="font-normal  messgae-content  text-lg md:text-sm  text-white ">${msg.message}</span>
     `
     msgContainer.appendChild(msgContent)
-
 }
 
-socket.on('message', () => {
-    appendChild(msg,  'incoming')
-
+socket.on('message', (msg) => {
+    appendMsg(msg, 'incoming-msg')  
 })
