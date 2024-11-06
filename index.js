46b6d5cc5c9c712cce29d391dd4081d28f0c951f
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-06 13:25:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const { log } = require('console')
 const express = require('express')
 const { dirname } = require('path')
 const app = express()
-// const io = require('socket.io')
 const http = require('http').createServer(app)
-const PORT = process.env.PORT || 3000
+const io = require('socket.io')(http)  
 
+const PORT = process.env.PORT || 3000
 
 app.use(express.static('public'))
 
-
-http.listen(PORT, () => {
-    console.log(`Server is running on port is ${PORT}`)
-})
-
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'pubic/index.html');
+    res.sendFile(__dirname + '/public/index.html') 
 })
 
-const io =  require('socket.io')(http)
+// Start the server
+http.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// Socket.io connection event
 io.on('connection', (socket) => {
-    console.log('a new connection is made')
+    // console.log('A new connection is made')
+
     socket.on('send', (msg) => {
-        // console.log(msg)
-        socket.broadcsast.emit('message',msg)
+        console.log('Received message:', msg)
+        socket.broadcast.emit('message', msg)  
     })
+
+ 
 })
