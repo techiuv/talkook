f7123e4d514551427637083e661bf5a82b7246f7
Yuvraj <160020119+techiuv@users.noreply.github.com>
2024-11-06 13:23:59 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/*html"],
+  content: ["./public/*html"],  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgcolor: {50: '#111'},  
+        maincolor: {50: '#b505fb'},
+        secondarycolor : {10: '#222'}
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        // Incoming message style
+        '.incoming-msg': {
+          float: 'left',
+          clear: 'both',
+        },
+        // Outgoing message style
+        '.outgoing-msg': {
+          float: 'right',
+          clear: 'both',
+        },
+        // Base message class
+        '.msg': {
+          padding: '10px',
+          margin: '10px',
+          borderRadius: '20px',
+          backgroundColor: '#b505fb',  
+          display: 'flex',
+          justifyContent: 'center',  
+          alignItems: 'center',  
+          position: 'relative'    
+        },
+      });
+    },
+  ],
 }
-
